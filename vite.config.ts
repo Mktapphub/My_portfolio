51cd8cc5858a72c9822283c0bfccd1885359625d
Minhajul Khan <128965778+Mktapphub@users.noreply.github.com>
2025-08-04 01:31:08 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"base: "/My_portfolio/",
+  base:"/My_portfolio/",
 
   server: {
     host: "::",
